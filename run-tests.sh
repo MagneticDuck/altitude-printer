fa8893e5d74d05f1e141feefca01d9247499b179
Chris Gadzinski <zenmags@gmail.com>
2015-11-08 21:24:57 +0000

    testing script

---
+++ run-tests.sh
@@ -0,0 +1,2 @@
+cat ./tests/flight-club.txt | ./dist/build/altitude-printer/altitude-printer > ./tests/flight-club-pretty.txt
+cat ./tests/tbg-log.txt | ./dist/build/altitude-printer/altitude-printer > ./tests/tbg-log-pretty.txt
